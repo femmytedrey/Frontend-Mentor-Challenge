fa2b30a08beef1004c9e1e54d8b528a1794e0c5d
femmytedrey <femmytedrey@gmail.com>
2023-08-26 00:43:28 +0100

    in progress

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ sun.addEventListener('click', () => {
     moon.style.display = 'block';
     sun.style.display = 'none';
 });
+
+const inputElement = document.getElementById('input-box');
+
+inputElement.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        
+        // Your submission logic here
+        console.log('Input submitted with value:', inputElement.value);
+        inputElement.value = ''
+    }
+});
